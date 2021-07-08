16bff591350819e353f46777fe12787203450820
germansayago <german.sayago@gmail.com>
2021-07-08 14:16:07 -0300

    feat/ was added bootstrap & bootstrap-vue and was added styles.scss

--- src/services/api.js
+++ src/services/api.js
@@ -1,23 +1,9 @@
 const API_URL = "https://admin.visitariocuarto.gob.ar/api/v1";
 
 function getEvents({ paginate = 9, query = null } = {}) {
-  // const pathUrl = filtered ? "?search=" : "/" + filtered + "/";
-
-  // console.log(pathUrl);
-  console.log(query);
-  //const apiUrl = `${API_URL}/events?paginate=${paginate}`;
-
   const apiUrl = query
     ? `${API_URL}/events${query}paginate=${paginate}`
     : `${API_URL}/events?paginate=${paginate}`;
-
-  console.log(apiUrl);
-  //console.log(filter);
-  // const apiUrl = filter
-  //   ? filter === "search"
-  //     ? `${API_URL}/events?search=${filter}&paginate=${paginate}`
-  //     : `${API_URL}/events?paginate=${paginate}`
-  //   : `${API_URL}/events?paginate=${paginate}`;
 
   return fetch(apiUrl)
     .then((res) => res.json())
@@ -27,7 +13,7 @@ function getEvents({ paginate = 9, query = null } = {}) {
 }
 function getEvent(id) {
   console.log(id);
-  return fetch(`${API_URL}/events?paginate=9`)
+  return fetch(`${API_URL}/events/${id}`)
     .then((res) => res.json())
     .then((res) => {
       return res.data;
